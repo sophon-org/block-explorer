008670eac04098fb0a8a5089efe5d5935b697007
consvic <cocoy_shuffle@me.com>
2024-10-28 11:54:18 -0600

    fix(chore): disable eslint any env.d.ts

--- packages/app/env.d.ts
+++ packages/app/env.d.ts
@@ -2,11 +2,13 @@
 
 declare module "*.vue" {
   import type { DefineComponent } from "vue";
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const component: DefineComponent<object, object, any>;
   export default component;
 }
 
 declare module "vue-i18n" {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const VueI18n: any;
   export default VueI18n;
   export * from "vue-i18n";
